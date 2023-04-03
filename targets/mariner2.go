7c83cd665b01c1dffb1080f06f998a4415b6a1e0
Peter Engelbert <pmengelbert@gmail.com>
2023-04-03 10:46:05 -0400

    Remove special mariner2-rpm target
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- targets/mariner2.go
+++ targets/mariner2.go
@@ -22,7 +22,7 @@ func Mariner2(ctx context.Context, client *dagger.Client, platform dagger.Platfo
 		return nil, err
 	}
 
-	t := &Target{client: client, c: c, platform: platform, name: "mariner2", pkgKind: "mariner2-rpm", buildPlatform: buildPlatform}
+	t := &Target{client: client, c: c, platform: platform, name: "mariner2", pkgKind: "rpm", buildPlatform: buildPlatform}
 	t, err = t.WithPlatformEnvs().InstallGo(ctx)
 	if err != nil {
 		return nil, err
