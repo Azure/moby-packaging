ea230681a1f30e53b0441c8e521807c0be3b7c83
Peter Engelbert <pmengelbert@gmail.com>
2023-05-10 16:43:28 -0400

    Fix bionic builds
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- targets/bionic.go
+++ targets/bionic.go
@@ -17,7 +17,7 @@ var (
 func Bionic(ctx context.Context, client *dagger.Client, platform dagger.Platform) (*Target, error) {
 	client = client.Pipeline("bionic/" + string(platform))
 	c := client.Container(dagger.ContainerOpts{Platform: platform}).From(BionicRef)
-	c = apt.Install(c, client.CacheVolume(BionicAptCacheKey), client.CacheVolume(BionicAptLibCacheKey), BaseDebPackages...)
+	c = apt.Install(c, client.CacheVolume(BionicAptCacheKey), client.CacheVolume(BionicAptLibCacheKey), BaseBionicPackages...)
 
 	buildPlatform, err := client.DefaultPlatform(ctx)
 	if err != nil {
