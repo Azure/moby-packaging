72e9638159aa9175273dc3d60d2678716bf1c5c5
Brian Goff <cpuguy83@gmail.com>
2023-06-07 23:49:18 +0000

    Make entrypoint script more resiliant to failure and add tracing

--- tests/docker-entrypoint.sh
+++ tests/docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -ex
+
 if [ ! -c /dev/kvm ]; then
     mknod /dev/kvm c 10 232
     chmod a+rw /dev/kvm
 fi
-rm /tmp/rootfs.qcow2 2>/dev/null
+[ ! -f /tmp/rootfs.qcow2 ] || rm /tmp/rootfs.qcow2
 qemu-img create -f qcow2 -b /tmp/rootfs-base.qcow2 -F qcow2 /tmp/rootfs.qcow2
 
 debug=""
