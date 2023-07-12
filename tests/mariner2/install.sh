1f96f7176174ceff695608aad7519f486dfa3e0f
Peter Engelbert <pmengelbert@gmail.com>
2023-07-12 14:06:32 -0400

    pipeline: Do not explicitly install tini on mariner
    tini should only be installed as a dependency of moby-engine.
    
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- tests/mariner2/install.sh
+++ tests/mariner2/install.sh
@@ -30,7 +30,6 @@ install() {
         moby-containerd-"${TEST_CONTAINERD_PACKAGE_VERSION}*" \
         moby-runc-"${TEST_RUNC_PACKAGE_VERSION}*" \
         moby-buildx-"${TEST_BUILDX_PACKAGE_VERSION}*" \
-        moby-tini-"${TEST_TINI_PACKAGE_VERSION}*" \
         moby-compose-"${TEST_COMPOSE_PACKAGE_VERSION}*"
 }
 
