7e0248b803d16f50f85439cb0ee6968eddb4e30c
Peter Engelbert <pmengelbert@gmail.com>
2023-07-25 12:58:45 -0400

    tests: fix glob pattern check in deb install
    Because the layout of the bundles directory has changed, the glob
    pattern ensuring that deb files are present must change to reflect this.
    
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- tests/deb/install.sh
+++ tests/deb/install.sh
@@ -19,7 +19,8 @@ prepare_local_apt() {
     if [ -n "${1}" ]; then
         dir="${1}"
     fi
-    if [ -z "$(ls ${dir}/*.deb)" ]; then
+
+    if [ -z "$(ls ${dir}/*/*.deb)" ]; then
         return
     fi
 
