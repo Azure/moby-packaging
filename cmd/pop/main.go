d69a1a539584ca1f021759b1f2223c42a93deba3
Peter Engelbert <pmengelbert@gmail.com>
2023-07-27 17:57:13 -0400

    pipeline: begin work on queue consumer
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

---
+++ cmd/pop/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+type downloadArgs struct {
+	outDir       string
+	messagesFile string
+}
+
+type uploadArgs struct {
+	inDir        string
+	messagesFile string
+}
+
+type deleteArgs struct {
+	messagesFile string
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return fmt.Errorf("available arguments are download, upload, and delete")
+	}
+
+	dlArgs := downloadArgs{}
+	upArgs := uploadArgs{}
+	dtArgs := deleteArgs{}
+	var messagesFile string
+
+	fs := flag.NewFlagSet("", flag.ExitOnError)
+	fs.StringVar(&dlArgs.outDir, "out-dir", "", "directory to download files")
+	fs.StringVar(&upArgs.inDir, "in-dir", "", "directory containing files to upload")
+	fs.StringVar(&messagesFile, "messages-file", "", "file containing queue messages to process")
+	fs.Parse(os.Args[2:])
+
+	dlArgs.messagesFile = messagesFile
+	upArgs.messagesFile = messagesFile
+	dtArgs.messagesFile = messagesFile
+
+	switch os.Args[1] {
+	case "download":
+		if err := runDownload(dlArgs); err != nil {
+			return err
+		}
+	case "upload":
+		if err := runUpload(upArgs); err != nil {
+			return err
+		}
+	case "delete":
+		if err := runDelete(dtArgs); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("available arguments are download, upload, and delete")
+	}
+
+	return nil
+}
+
+func runDownload(args downloadArgs) error {
+
+	fmt.Printf("%#v\n", args)
+	return nil
+}
+
+func runUpload(args uploadArgs) error {
+	_ = args
+
+	fmt.Println(args)
+	return nil
+}
+
+func runDelete(args deleteArgs) error {
+	_ = args
+
+	fmt.Println(args)
+	return nil
+}
