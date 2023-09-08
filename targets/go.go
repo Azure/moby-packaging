dbffc82e7e250bb021adf77711d69e9bc8cf0123
Brian Goff <cpuguy83@gmail.com>
2023-09-08 06:41:49 -0700

    Bump go to 1.20.8 (#117)

--- targets/go.go
+++ targets/go.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	GoVersion     = "1.20.7"
+	GoVersion     = "1.20.8"
 	GoRef         = path.Join("mcr.microsoft.com/oss/go/microsoft/golang:" + GoVersion)
 	GoModCacheKey = "go-mod-cache"
 )
