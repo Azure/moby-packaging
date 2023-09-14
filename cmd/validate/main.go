0edbea5daf730f3bc7abd172d38ad0366c0f4d63
Peter Engelbert <pmengelbert@gmail.com>
2023-09-14 12:11:30 -0400

    Refactor validation and pipeline
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- cmd/validate/main.go
+++ cmd/validate/main.go
@@ -3,33 +3,48 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/Azure/moby-packaging/pkg/archive"
 )
 
-func main() {
-	pkgName := ""
-	if len(os.Args) > 1 {
-		pkgName = os.Args[1]
-	}
-	r := bufio.NewReader(os.Stdin)
+type allArgs struct {
+	pkg      string
+	tag      string
+	revision string
+}
 
-	j, err := r.ReadBytes(byte(0))
-	if err != nil && err != io.EOF {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+func main() {
+	args := allArgs{}
+	flag.StringVar(&args.pkg, "project", "", "name of project")
+	flag.StringVar(&args.tag, "tag", "", "tag for build set")
+	flag.StringVar(&args.revision, "revision", "", "revision for build set")
+	flag.Parse()
+
+	if args.pkg == "" || args.tag == "" || args.revision == "" {
+		flag.Usage()
 		os.Exit(1)
 	}
 
-	if err := validate(j, pkgName); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+	if err := validate(args); err != nil {
+		fmt.Fprintf(os.Stderr, "##vso[task.logissue type=error;]%s\n", err)
 		os.Exit(1)
 	}
 }
 
-func validate(j []byte, pkgName string) error {
+func validate(args allArgs) error {
+	r := bufio.NewReader(os.Stdin)
+
+	j, err := r.ReadBytes(byte(0))
+	if err != nil && err != io.EOF {
+		return fmt.Errorf("error: %s\n", err)
+	}
+
 	specs := []archive.Spec{}
 
 	if err := json.Unmarshal(j, &specs); err != nil {
@@ -37,23 +52,28 @@ func validate(j []byte, pkgName string) error {
 	}
 
 	for i := range specs {
-		if pn := specs[i].Pkg; pn != pkgName {
-			return fmt.Errorf("package name does not match: '%s' vs '%s'", pkgName, pn)
+		if project := specs[i].Pkg; project != args.pkg {
+			return fmt.Errorf("package name does not match: was '%s', should be '%s'", args.pkg, project)
 		}
 
-		s := []string{
-			specs[i].Arch,
-			specs[i].Commit,
-			specs[i].Distro,
-			specs[i].Pkg,
-			specs[i].Repo,
-			specs[i].Revision,
-			specs[i].Tag,
+		if tag := specs[i].Tag; tag != args.tag {
+			return fmt.Errorf("package tag does not match: was '%s', should be '%s'", tag, args.tag)
 		}
 
-		for _, ss := range s {
-			if ss == "" {
-				return fmt.Errorf("blank value")
+		if revision := specs[i].Revision; revision != args.revision {
+			return fmt.Errorf("package revision does not match: was '%s', should be '%s'", revision, args.revision)
+		}
+
+		v := reflect.ValueOf(&specs[i]).Elem()
+		for i := 0; i < v.NumField(); i++ {
+			f := v.Field(i).Interface().(string)
+
+			if f == "" {
+				return fmt.Errorf("blank value: %s", v.Type().Field(i).Name)
+			}
+
+			if strings.Contains(f, "'") {
+				return fmt.Errorf(`illegal character in field '%s': "%s"`, v.Type().Field(i).Name, f)
 			}
 		}
 	}
