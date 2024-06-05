278e85592e13faaa5feace6d6f6e1ce4a23db24d
Brian Goff <cpuguy83@gmail.com>
2024-06-05 19:10:06 +0000

    Fix go version for containerd 2.0 builds
    containerd 2.0 now requires go1.22 to build so we need to handle this as
    a special case.
    
    Note: go1.22 is broken on engine and runc (at least) and still needs to
    be addressed upstream.

--- packages/moby-containerd/go_version.go
+++ packages/moby-containerd/go_version.go
@@ -3,8 +3,18 @@ package containerd
 import (
 	"github.com/Azure/moby-packaging/pkg/archive"
 	"github.com/Azure/moby-packaging/pkg/goversion"
+	"github.com/Masterminds/semver/v3"
 )
 
-func GoVersion(_ *archive.Spec) string {
-	return goversion.DefaultVersion
+func GoVersion(spec *archive.Spec) string {
+	v, err := semver.NewVersion(spec.Tag)
+	if err != nil {
+		panic(err)
+	}
+
+	if v.Major() < 2 {
+		return goversion.OneTwentyOne
+	} else {
+		return goversion.OneTwentyTwo
+	}
 }
