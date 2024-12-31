3cd4f438e1fd394cd09bac76dd48f22b29f3f1ab
Brian Goff <cpuguy83@gmail.com>
2024-12-31 10:10:23 -0800

    runc: set COMMIT via make arg (#219)
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- packages/moby-runc/deb.mk
+++ packages/moby-runc/deb.mk
@@ -2,7 +2,7 @@ deb: runc man/man8
 
 runc:
 	cd src && \
-	$(MAKE) runc BUILDTAGS='seccomp' VERSION="${VERSION}-${REVISION}"
+	$(MAKE) runc BUILDTAGS='seccomp urfave_cli_no_docs' VERSION="${VERSION}-${REVISION}" COMMIT="${COMMIT}"
 
 man/man8:
 	cd src && \
